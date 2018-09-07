7ca720859ed8091283407736eac15be4c141f1e3
shanmugapriya003 <43057340+shanmugapriya003@users.noreply.github.com>
2018-09-07 11:14:08 +0530

    Add files via upload

---
+++ maths calculation.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    a=13;
+    b=15;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("the sum is"+c);
+    System.out.println("the difference is"+d);
+    System.out.println("the multiply is"+e);
+    System.out.println("the divided is"+f);
+  }
+}
