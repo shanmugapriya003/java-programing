30a32e51d3a318517bcf97470b7d959f5d34c02a
shanmugapriya003 <43057340+shanmugapriya003@users.noreply.github.com>
2018-10-03 12:13:48 +0530

    Add files via upload

---
+++ number is positive or negative or zero.java
@@ -0,0 +1,14 @@
+class Main{
+ public static void main(String args[]){
+ int i=6;
+ if(i>0)
+ {
+  System.out.println(i+"positive");
+ }
+ else 
+ {
+  System.out.println(i+"negative");
+  }
+ }
+}
+
