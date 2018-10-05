343193e8f5009081a66f51b99d4c5d4f8455cdb3
shanmugapriya003 <43057340+shanmugapriya003@users.noreply.github.com>
2018-10-05 12:03:16 +0530

    Add files via upload

---
+++ char is an alpabet or not .java
@@ -0,0 +1,9 @@
+class Main  {
+public static void main(String[] args) {
+char c = 's';
+if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+ System.out.println(c + " is an alphabet.");
+ else
+System.out.println(c + " is not an alphabet.");
+    }
+}
